33dfd3a5da4ba4900fa84057a0b0c66b3180ab18
Kavisha Behrani <kavishabehrani@Kavishas-MacBook-Air.local>
2022-02-25 05:44:59 -0500

    dd

---
+++ test_new.py
@@ -0,0 +1,8 @@
+#print("hello")
+
+import pandas as pd
+# print(pd.__version__)
+#df=pd.read_csv('~/Documents/Python_files/test_file.csv')
+df=pd.read_csv('~/Documents/Python_files/test_file1.csv')
+print(df)
+ 
